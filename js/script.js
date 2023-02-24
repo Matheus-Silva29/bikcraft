85f31bef25deff1f65e102df8bd0966df2110ac5
Matheus-Silva29 <cmatheusdeveloper@gmail.com>
2023-02-24 18:35:07 -0300

    Finalizando-projeto

--- js/script.js
+++ js/script.js
@@ -45,3 +45,28 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// GALERIA DE BICICLETAS
+
+const galeria = document.querySelectorAll(".bicicleta-imagem img");
+const galeriaContainer = document.querySelector(".bicicleta-imagem");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+//ANIMAÇÃO
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
